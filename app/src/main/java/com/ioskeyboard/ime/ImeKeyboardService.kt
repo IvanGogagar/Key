2309be7ce790d2799eb3ea6a5d421e04ddbfcb77
IvanGogagar <iwenovik228@gmail.com>
2026-09-08 10:24:56 +0700

    due77778

--- app/src/main/java/com/ioskeyboard/ime/ImeKeyboardService.kt
+++ app/src/main/java/com/ioskeyboard/ime/ImeKeyboardService.kt
@@ -3,23 +3,40 @@ package com.ioskeyboard.ime
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.inputmethodservice.InputMethodService
+import android.widget.FrameLayout
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.setViewTreeLifecycleOwner
 import com.ioskeyboard.model.KeyboardAction
 import com.ioskeyboard.ui.KeyboardScreen
 import com.ioskeyboard.ui.KeyboardViewModel
 import com.ioskeyboard.ui.theme.IOSStyleKeyboardTheme
 import kotlinx.coroutines.flow.collectLatest
 
-class ImeKeyboardService : InputMethodService() {
+class ImeKeyboardService : InputMethodService(), LifecycleOwner {
 
-    private var composeView: ComposeView? = null
+    private var container: FrameLayout? = null
+    private val lifecycleRegistry = LifecycleRegistry(this)
     private val viewModel by lazy { KeyboardViewModel() }
 
-    override fun onCreateInputView(): View {
-        composeView?.disposeComposition()
+    override val lifecycle: Lifecycle get() = lifecycleRegistry
 
-        composeView = ComposeView(this).apply {
+    override fun onCreate() {
+        super.onCreate()
+        lifecycleRegistry.currentState = Lifecycle.State.CREATED
+    }
+
+    override fun onCreateInputView(): View {
+        lifecycleRegistry.currentState = Lifecycle.State.STARTED
+
+        container = FrameLayout(this).apply {
+            setViewTreeLifecycleOwner(this@ImeKeyboardService)
+        }
+
+        val composeView = ComposeView(this).apply {
             setContent {
                 IOSStyleKeyboardTheme {
                     LaunchedEffect(Unit) {
@@ -39,7 +56,10 @@ class ImeKeyboardService : InputMethodService() {
                 }
             }
         }
-        return composeView!!
+
+        container!!.addView(composeView)
+        lifecycleRegistry.currentState = Lifecycle.State.RESUMED
+        return container!!
     }
 
     private fun sendKeyEvent(keyCode: Int, label: String) {
@@ -54,14 +74,22 @@ class ImeKeyboardService : InputMethodService() {
 
     override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
         super.onStartInputView(info, restarting)
+        lifecycleRegistry.currentState = Lifecycle.State.RESUMED
     }
 
     override fun onFinishInputView(finishingInput: Boolean) {
         super.onFinishInputView(finishingInput)
+        lifecycleRegistry.currentState = Lifecycle.State.STARTED
     }
 
     override fun onDestroy() {
-        composeView?.disposeComposition()
+        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+        container?.let {
+            for (i in 0 until it.childCount) {
+                val child = it.getChildAt(i)
+                if (child is ComposeView) child.disposeComposition()
+            }
+        }
         super.onDestroy()
     }
 }
